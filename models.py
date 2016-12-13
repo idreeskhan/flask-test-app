271e7560e405dd0b085aeb3df50add3d0520a454
Idrees Khan <idrees.x.khan@gmail.com>
2016-12-12 19:12:05 -0500

    Quick model bug fixes

--- models.py
+++ models.py
@@ -8,44 +8,47 @@ class Teacher(object):
         self.create_time = create_time
         self.age = age
 
+
 class Student(object):
     columns = ['id', 'create_time', 'name', 'age']
     table = 'students'
 
-    def __init__(self, name, id = None, create_time = None, age = None):
+    def __init__(self, name, id=None, create_time=None, age=None):
         self.name = name
         self.id = id
         self.create_time = create_time
         self.age = age
 
+
 class Course(object):
     columns = ['id', 'create_time', 'code', 'department']
-    table = courses
+    table = 'courses'
 
-    def __init__(self, id = None, create_time = None, code, department):
+    def __init__(self, code, department, id=None, create_time=None):
         self.id = id
         self.create_time = create_time
         self.code = code
         self.department = department
 
-class Student_Course_Map(object):
+
+class StudentCourseMap(object):
     columns = ['id', 'create_time', 'student_id', 'course_id', 'grade']
     table = 'student_course_mapping'
 
-    def __init__(self, id = None, create_time = None, student_id, course_id, grade):
+    def __init__(self, student_id, course_id, grade, id=None, create_time=None):
         self.id = id
         self.create_time = create_time
-        slf.student_id = student_id
+        self.student_id = student_id
         self.course_id = course_id
         self.grade = grade
 
-class Teacher_Course_Map(object):
+
+class TeacherCourseMap(object):
     columns = ['id', 'create_time', 'teacher_id', 'course_id']
     table = 'teacher_course_mapping'
 
-    def __init__(self, id = None, create_time = None, teacher_id, course_id, grade):
+    def __init__(self, teacher_id, course_id, id=None, create_time=None):
         self.id = id
         self.create_time = create_time
         self.teacher_id = teacher_id
         self.course_id = course_id
-        
